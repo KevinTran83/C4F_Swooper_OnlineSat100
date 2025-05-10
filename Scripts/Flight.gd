2bbd2d908eb817d9205a97c3d6283beec42429f1
Kevin S. Tran <tanksense@gmail.com>
2025-05-10 14:01:12 +1000

    Week 2

--- Scripts/Flight.gd
+++ Scripts/Flight.gd
@@ -1,6 +1,8 @@
+class_name Flight
 extends Node
 
 @export var speed : float = 10
+@export var maxSpeed : float = 40
 
 @onready var body : CharacterBody3D = get_parent()
 
@@ -8,3 +10,7 @@ func _physics_process(delta: float) -> void:
     var velocity : Vector3 = -body.global_basis.z * speed
     body.velocity = velocity
     body.move_and_slide()
+    
+func accelerate(amount : float) -> void:
+    speed += amount
+    speed = clampf(speed, 0, maxSpeed)
---
+++ Scripts/Turn.gd
@@ -0,0 +1,35 @@
+extends Node
+
+@onready var body : CharacterBody3D = get_parent()
+@onready var flight : Flight = get_node("../Flight")
+
+@export var mesh : MeshInstance3D
+
+@export var damp : float = 10
+
+var speed : float
+
+func _ready() -> void:
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    speed = 0
+    
+func _process(delta : float) -> void:
+    var mouseX = Input.get_last_mouse_velocity().x
+    var mouseY = Input.get_last_mouse_velocity().y
+    
+    # Accelerate turn speed
+    speed += mouseX/damp * delta
+    speed = clampf(speed, -50, 50)
+    
+    speed = lerpf(speed, 0, 1 * delta) # Reduce turn speed
+    
+    body.rotate_y( -deg_to_rad(speed) * delta )
+    body.rotate_object_local(Vector3.RIGHT, -deg_to_rad(mouseY/damp) * delta)
+    
+    # Accelerate flight speed based on pitch (x rotation)
+    var pitch : float = rad_to_deg(body.get_rotation().x)
+    if pitch > 0 : flight.accelerate(pitch/2 * delta)
+    else         : flight.accelerate(pitch/5 * delta)
+    
+    if mesh : mesh.rotation.z = -deg_to_rad(speed) # TODO: Don't change the
+                                                   #       variable directly.
